9aeffdedc5e2801b760de7b3866e26d10d089078
sagar_kushwaha <138853235+sagar0101@users.noreply.github.com>
2023-11-16 19:18:45 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:[" ","Web Designer","Ui-Ux Designer","Web Developer","Coder"],
+    typeSpeed :100,
+    BackSpeed :60,
+    loop:true
+
+});
